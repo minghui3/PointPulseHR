80c5c76454584190db3882c11696011713f64782
minghui3 <s10257917@connect.np.edu.sg>
2024-12-02 21:48:57 +0800

    updated driver

--- maven/src/test/java/stepdefinitions/DriverManager.java
+++ maven/src/test/java/stepdefinitions/DriverManager.java
@@ -1,17 +1,14 @@
-package stepdefinitions;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 public class DriverManager {
-    private static RemoteWebDriver driver;
+    private static WebDriver driver;
 
-    public static RemoteWebDriver getDriver() {
+    public static WebDriver getDriver() {
         if (driver == null) {
             initializeDriver();
         }
@@ -22,27 +19,30 @@ public class DriverManager {
         // Get the browser type from the system property, default to Chrome
         String browser = System.getProperty("browser", "chrome").toLowerCase();
 
-        switch (browser) {
-            case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                driver = new ChromeDriver(chromeOptions);
-                break;
+        try {
+            String hubUrl = "http://localhost:4444/wd/hub";  // URL of the Selenium Grid Hub
 
-            case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                driver = new FirefoxDriver(firefoxOptions);
-                break;
+            switch (browser) {
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    driver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
+                    break;
 
-            case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--headless"); // Use headless mode
-                edgeOptions.addArguments("--disable-gpu"); // Disable GPU acceleration
-                edgeOptions.addArguments("--remote-debugging-port=4444");
-                driver = new EdgeDriver(edgeOptions);
-                break;
+                case "firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    driver = new RemoteWebDriver(new URL(hubUrl), firefoxOptions);
+                    break;
 
-            default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
+                case "edge":
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    driver = new RemoteWebDriver(new URL(hubUrl), edgeOptions);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Unsupported browser: " + browser);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
