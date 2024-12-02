b5d604ede0c8c50e3fe81824ac9c1a45c8e4a151
minghui3 <s10257917@connect.np.edu.sg>
2024-12-02 23:39:07 +0800

    reverted driver manager

--- maven/src/test/java/stepdefinitions/DriverManager.java
+++ maven/src/test/java/stepdefinitions/DriverManager.java
@@ -1,12 +1,12 @@
 package stepdefinitions;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
     private static WebDriver driver;
@@ -20,36 +20,26 @@ public class DriverManager {
 
     private static void initializeDriver() {
         // Get the browser type from the system property, default to Chrome
-        String browser = System.getProperty("browser", "chrome").toLowerCase();
+        String browser = System.getProperty("browser", "edge").toLowerCase();
 
-        try {
-            String hubUrl = "http://selenium-hub:4444/";  // URL of the Selenium Grid Hub
+        switch (browser) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                driver = new ChromeDriver(chromeOptions);
+                break;
 
-            switch (browser) {
-                case "chrome":
-                    ChromeOptions chromeOptions = new ChromeOptions();
-                    driver = new RemoteWebDriver(new URL(hubUrl), chromeOptions);
-                    break;
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
 
-                case "firefox":
-                    FirefoxOptions firefoxOptions = new FirefoxOptions();
-                    driver = new RemoteWebDriver(new URL(hubUrl), firefoxOptions);
-                    break;
+            case "edge":
+                EdgeOptions edgeOptions = new EdgeOptions();
+                driver = new EdgeDriver(edgeOptions);
+                break;
 
-                case "edge":
-                    EdgeOptions edgeOptions = new EdgeOptions();
-                    driver = new RemoteWebDriver(new URL(hubUrl), edgeOptions);
-                    break;
-
-                default:
-                    throw new IllegalArgumentException("Unsupported browser: " + browser);
-            }
-            
-
-        } catch (Exception e) {
-            System.out.println("Error initializing WebDriver: " + e.getMessage());
-            e.printStackTrace();
-            throw new RuntimeException("Failed to initialize WebDriver", e);  // Re-throw exception for better handling
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
     }
 
